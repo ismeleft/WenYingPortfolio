3149d4b3b09259dc8ddb8558b3c98970a73c65d3
ying <leftleft0813@gmail.com>
2023-12-30 11:27:36 +0800

    style: RWD design for project & contact

--- Components/Contact/Contact.js
+++ Components/Contact/Contact.js
@@ -1,23 +1,31 @@
 import React from "react";
 import Image from "next/image";
+
 const Contact = () => {
   return (
-    <div className="my-0 mx-auto p-10" id="contact">
-      <div className="text-center">Get in Touch </div>
-      <h1 className="text-4xl font-bold text-center">Contact Me</h1>
-      <div className="border rounded-3xl border-black p-2 mt-10 gap-3 w-1/4 text-center flex justify-center  items-center my-0 mx-auto ">
+    <div className="my-0 mx-auto p-5 md:p-10" id="contact">
+      {/* Responsive text size for the subheading */}
+      <div className="text-center text-lg md:text-xl">Get in Touch </div>
+      {/* Responsive text size and margin for the heading */}
+      <h1 className="text-3xl md:text-4xl font-bold text-center mt-2 md:mt-4">
+        Contact Me
+      </h1>
+      {/* Responsive width, padding, and margin for the contact card */}
+      <div className="border rounded-3xl border-black p-4 md:p-6 mt-8 md:mt-10 gap-3 w-3/4 md:w-1/2 lg:w-1/4 text-center flex justify-center items-center mx-auto">
         <a
           href="mailto:leftleft0813@gmail.com"
-          className="flex justify-center  items-center"
+          className="flex justify-center items-center"
         >
+          {/* Responsive image size */}
           <Image
             src={"/email.png"}
-            width={50}
-            height={50}
+            width={40}
+            height={40}
             alt="email icon"
-            className="m-2"
+            className="m-1 md:m-2"
           ></Image>
-          <p>leftleft0813@gmail.com</p>
+          {/* Responsive text size for the email */}
+          <p className="text-sm md:text-base">leftleft0813@gmail.com</p>
         </a>
       </div>
     </div>
--- Components/Projects/Projects.js
+++ Components/Projects/Projects.js
@@ -4,28 +4,28 @@ import Image from "next/image";
 const Projects = () => {
   return (
     <div>
-      <div className="text-center p-10" id="projects">
+      <div className="text-center p-10 md:p-20" id="projects">
         <div>Browse My </div>
-        <h1 className="text-4xl font-bold text-center">Projects</h1>
+        <h1 className="text-2xl md:text-4xl font-bold text-center">Projects</h1>
       </div>
-      <div className="flex justify-center gap-5">
-        <div className="border rounded-3xl border-black p-6 gap-3 w-1/3 text-center flex flex-col items-center">
+      <div className="p-6 flex flex-col md:flex-row justify-center gap-5">
+        <div className="border rounded-3xl border-black p-4 md:p-6 gap-3 w-full md:w-1/3 text-center flex flex-col items-center">
           <Image
             src={"/1.png"}
-            width={400}
-            height={400}
+            width={300}
+            height={300}
             alt="coding icon"
             className="m-2"
           ></Image>
-          <h1 className="text-2xl font-bold ">GratifyME</h1>
+          <h1 className="text-xl md:text-2xl font-bold ">GratifyME</h1>
           <p>A Gratitude Journal Web Application</p>
-          <div className="mt-2 flex gap-4">
-            <button className="border rounded-3xl border-black p-3">
+          <div className="mt-2 flex flex-col md:flex-row gap-4">
+            <button className="border rounded-3xl border-black p-2 md:p-3">
               <a target="_blank" href="https://gratifyme.vercel.app/">
                 Live Demo
               </a>
             </button>
-            <button className="border rounded-3xl border-black p-3 bg-black">
+            <button className="border rounded-3xl border-black p-2 md:p-3 bg-black">
               <a
                 target="_blank"
                 href="https://github.com/ismeleft/gratifyme"
@@ -36,23 +36,24 @@ const Projects = () => {
             </button>
           </div>
         </div>
-        <div className="border rounded-3xl border-black p-6 gap-3 w-1/3 text-center flex flex-col items-center">
+
+        <div className="border rounded-3xl border-black p-4 md:p-6 gap-3 w-full md:w-1/3 text-center flex flex-col items-center">
           <Image
             src={"/2.png"}
-            width={400}
-            height={400}
+            width={300}
+            height={300}
             alt="coding icon"
             className="m-2"
           ></Image>
-          <h1 className="text-2xl font-bold ">Taipei-Day-Trip</h1>
+          <h1 className="text-xl md:text-2xl font-bold">Taipei-Day-Trip</h1>
           <p>An e-commerce website about Taipei tourism.</p>
-          <div className="mt-2 flex gap-4">
-            <button className="border rounded-3xl border-black p-3">
+          <div className="mt-2 flex flex-col md:flex-row gap-4">
+            <button className="border rounded-3xl border-black p-2 md:p-3">
               <a target="_blank" href="http://52.192.139.251:3000/">
                 Live Demo
               </a>
             </button>
-            <button className="border rounded-3xl border-black p-3 bg-black">
+            <button className="border rounded-3xl border-black p-2 md:p-3 bg-black">
               <a
                 target="_blank"
                 href="https://github.com/ismeleft/taipei-day-trip"
