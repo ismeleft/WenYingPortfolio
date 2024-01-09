45824b524bdc7cfaa5f41b92261280a2286b19cc
ying <leftleft0813@gmail.com>
2024-01-09 17:25:22 +0800

    add favicon

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata = {
   title: "Wen Ying Portfolio",
   description: "Wen Ying Portfolio",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({ children }) {
