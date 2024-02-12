cd3676aeb8987fb10cb763728c5e8666ea9cd61e
ying <leftleft0813@gmail.com>
2024-02-12 14:46:59 +0800

    change footer copyright year

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="text-center p-10">
-      Copyright © 2023 WenYing. All rights reserved.
+      Copyright © 2024 WenYing. All rights reserved.
     </div>
   );
 };
