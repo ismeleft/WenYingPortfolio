6f4607b75d50712b1659876db338d082aae76c64
ying <leftleft0813@gmail.com>
2024-02-15 12:55:03 +0800

    style: nav component

--- Components/Nav/Nav.js
+++ Components/Nav/Nav.js
@@ -5,20 +5,22 @@ import Link from "next/link";
 
 export const Nav = () => {
   return (
-    <nav className="fixed top-0 left-0 right-0 z-10 mx-auto max-w-[1200px] px-8 py-3">
-      <div className="flex items-center justify-between">
-        <h1 className="cursor-pointer">
-          <Link href={"/"}>Yings</Link>
-        </h1>
-        <div>
-          <button className="mr-4 text-xl cursor-pointer hover:text-red-600">
-            <Link href={"/blog"}>Blog</Link>
-          </button>
-          <button className="text-xl cursor-pointer hover:text-red-600">
-            <Link href={"/changelog"}>Changelog</Link>
-          </button>
+    <div className="z-10 sticky top-0 bg-yellow-50 bg-opacity-50 backdrop-blur-md ">
+      <nav className=" mx-auto max-w-[1200px] px-8 py-3">
+        <div className="flex items-center justify-between">
+          <h1 className="cursor-pointer">
+            <Link href={"/"}>Yings</Link>
+          </h1>
+          <div>
+            <button className="mr-4 text-xl cursor-pointer hover:text-red-600">
+              <Link href={"/blog"}>Blog</Link>
+            </button>
+            <button className="text-xl cursor-pointer hover:text-red-600">
+              <Link href={"/changelog"}>Changelog</Link>
+            </button>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
