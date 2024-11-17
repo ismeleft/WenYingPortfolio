481b63602911ae50c49ec0e4e5f5a51d91d03b6f
ying <leftleft0813@gmail.com>
2024-11-17 13:14:25 +0800

    feat: 新增 SEO 相關設定

---
+++ app/sitemap.js
@@ -0,0 +1,25 @@
+// app/sitemap.ts
+import { MetadataRoute } from "next";
+
+export default async function sitemap() {
+  return [
+    {
+      url: "https://wen-ying-portfolio.vercel.app",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://wen-ying-portfolio.vercel.app/blog",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+    {
+      url: "https://wen-ying-portfolio.vercel.app/changelog",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.5,
+    },
+  ];
+}
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,27 @@
 module.exports = {
   siteUrl: "https://wen-ying-portfolio.vercel.app/",
   generateRobotsTxt: true,
+  // 更改 sitemap 產生頻率
+  changefreq: "daily",
+  // 設定優先級
+  priority: 0.7,
+  // 排除不需要的頁面
+  exclude: ["/admin/*", "/private/*"],
+  // 產生額外的 sitemap
+  additionalSitemaps: [
+    "https://wen-ying-portfolio.vercel.app/server-sitemap.xml",
+  ],
+  // 自定義 robots.txt 內容
+  robotsTxtOptions: {
+    additionalSitemaps: ["https://wen-ying-portfolio.vercel.app/sitemap.xml"],
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/admin", "/private"],
+      },
+    ],
+  },
+  // 設定需要產生 sitemap 的路徑
+  sitemapSize: 5000,
 };
