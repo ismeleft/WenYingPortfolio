cf681e5fdd65f970ebe19f79cd8917f7611b0704
ying <leftleft0813@gmail.com>
2026-02-15 11:53:14 +0800

    feat: 新增 projects 資料設定檔與縮圖目錄

---
+++ data/projects.js
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    id: "gratifyme",
+    title: "GratifyME",
+    description: "A Gratitude Journal Web Application",
+    thumbnail: "/projects/gratifyme.png",
+    techStack: ["Next.js", "Tailwind CSS", "Supabase"],
+    liveDemo: "https://gratifyme.vercel.app/",
+    github: "https://github.com/ismeleft/gratifyme",
+  },
+  {
+    id: "taipei-day-trip",
+    title: "Taipei-Day-Trip",
+    description: "An e-commerce website about Taipei tourism.",
+    thumbnail: "/projects/taipei-day-trip.png",
+    techStack: ["Python", "FastAPI", "MySQL", "JavaScript"],
+    liveDemo: "http://52.192.139.251:3000/",
+    github: "https://github.com/ismeleft/taipei-day-trip",
+  },
+];
